001c8414ba99eff5f2e202c12e298cb63ed1f658
hehuiqi <253956882@qq.com>
2017-07-24 19:28:49 +0800

    国际化

---
+++ src/com/hhq/study/HqInternationalization/HqInternationalTest.java
@@ -0,0 +1,17 @@
+package com.hhq.study.HqInternationalization;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hehuiqi on 7/24/17.
+ */
+public class HqInternationalTest {
+    @Test
+    public void internationalTest(){
+
+        new HqInternationnalUse().myinternational();
+    }
+
+}
